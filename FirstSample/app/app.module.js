8f7de74e7e056603f5010c213a027765487fa559
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-07-20 20:25:39 +0530

    ..

--- FirstSample/app/app.module.js
+++ FirstSample/app/app.module.js
@@ -1,9 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var first_component_1 = require("./first.component");
 // define the `phonecatApp` module
-angular.module("sampleApp", [
-    "firstPage"
-])
-    .controller("FirstController", first_component_1.FirstController);
+angular.module("sampleApp", [])
+    .controller("FirstController", FirstController);
 //# sourceMappingURL=app.module.js.map
--- FirstSample/app/app.module.ts
+++ FirstSample/app/app.module.ts
@@ -1,9 +1,5 @@
 "use strict";
 
-import { FirstController } from "./first.component";
-
 // define the `phonecatApp` module
-angular.module("sampleApp", [
-  "firstPage"
-])
-.controller("FirstController", FirstController);
+angular.module("sampleApp", [])
+  .controller("FirstController", FirstController);
