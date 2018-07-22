6aac8f8fb021974c3f89a340884758be1a65d43a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-07-22 18:52:51 +0530

    ..

--- FirstSample/app/first.component.js
+++ FirstSample/app/first.component.js
@@ -1,7 +1,1 @@
-"use strict";
-angular.module("sampleApp", []).controller('PersonController', PersonController);
-PersonController.$inject = ['$scope'];
-function PersonController($scope) {
-    $scope.age = '25';
-}
 //# sourceMappingURL=first.component.js.map
