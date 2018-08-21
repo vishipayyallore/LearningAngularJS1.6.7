ca232428bc19048123738541b3bb8567931aef34
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-08-21 10:15:08 +0530

    Working on the sample

--- SampleApplication/app/app.module.ts
+++ SampleApplication/app/app.module.ts
@@ -1,5 +1,26 @@
-module app {
+(function () {
 
-    
-
-}
+    angular.module('app', [])
+      .controller('HelloController', HelloController);
+  
+    HelloController.$inject = ['$scope'];
+  
+    function HelloController($scope) {
+      $scope.name = 'Shiva';
+  
+      $scope.student = {
+        firstName: 'Shiva',
+        lastName: 'Sai',
+  
+        getFullName: function () {
+          return this.firstName + " " + this.lastName;
+        }
+      };
+  
+      $scope.getName = function () {
+        return this.name;
+      };
+  
+    }
+  
+  })();
